a9635f581bee200992ec42a141f798073e85b144
ThibBal <thibault.balmette@isep.fr>
2014-03-12 10:49:52 +0100

    Passage au php

---
+++ Site/accueil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style.css" />
+	<link rel="icon" href="img/favicon.ico" />
+	<title>Highway To Bands</title>    
+</head>
+
+<body>
+	<?php include 'header.php' ?>
+
+	<div class="vide"><p>Ici il y aura une quelque chose (mais quoi...)</p></div>
+
+	<div id="contenu">
+		<div class="article1">
+			<h1 id="titre_categorie">Votre fil d'actualité</h1>
+			<p><a href="#">Breaking News : Les L5 se reforment, pour un énième come-back</a></p>
+			<p><a href='#'>Scoop : Michael Jackson n'est pas mort</a></p>
+			<p><a href='#'>Inédit : John Lennon est bien mort</a></p>
+		</div>
+
+
+		<div class="article1">
+			<h1 id="titre_categorie">Vos évènements</h1>
+			<p><a href="#">Festival Rock en Seine : 22 Août 2014</a></p>
+			<p><a href="#">Showcase privé de Trioncé : 29 Septembre 2014</a></p>
+			<p><a href="#">Concert hommage à Claude François : 24 Décembre 2014</a></p>
+		</div>
+
+		<div class="article1">
+			<h1 id="titre_categorie">Vos photos récentes</h1>
+			<p><center><img width=150 src="img/photo1.jpg"></center></p>
+			<p><center><img width=150 src="img/photo2.jpg"></center></p>
+			<p><center><img width=150 src="img/photo3.JPG"></center></p>
+		</div>
+	</div>
+
+	<div class="vide"><p>Ici il y aura une quelque chose (mais quoi...)</p></div>
+	<footer>
+		<a href="faq.html">FAQ</a></li>
+		<a href="legales.html">Mentions légales</a></li>
+	</footer>		
+</body>
+</html>
---
+++ Site/header.php
@@ -0,0 +1,47 @@
+<header>
+
+		<div id="logo"><center><a href="accueil.php"><img width=150 src="img/logo.png" alt="Logo du site"></a></center></div>
+		<a href="#"><div id="connexion"><p>Identification</p>
+		</div></a>
+
+		<div id="cadre">
+		</div>
+
+		
+			<div id="menu">
+				<ul>
+					<li><a href="event.html">Evènements</a>
+						<ul>
+							<li><a href="#">Les + populaires</a></li>
+							<li><a href="#">Ordre alphabétique</a></li>
+							<li><a href="#">Les + récents</a></li>
+						</ul>
+					</li>
+					<li><a href="salle.html">Salles</a>
+						<ul>
+							<li><a href="#">Les + populaires</a></li>
+							<li><a href="#">Ordre alphabétique</a></li>
+							<li><a href="#">Proche de chez vous</a></li>
+						</ul>
+					</li>
+					<li><a href="groupe.html">Groupes</a>
+						<ul>
+							<li><a href="#">Les + populaires</a>
+							</li>
+							<li><a href="#">Ordre alphabétique</a>
+							</li>
+							<li><a href="#">Par style de musique</a>
+							</li>
+						</ul>
+					</li>
+					<li><a href="forum.html">Forum</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+
+		<div id="search">
+			
+		</div>	
+		
+		
+	</header>
