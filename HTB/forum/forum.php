c42ebbf0e7972f3fe26e18e8135362f5a6e6b8f2
cesarfranck <aamselem@isep.fr>
2014-04-28 17:13:59 +0200

    forum

---
+++ HTB/forum/forum.php
@@ -0,0 +1,194 @@
+<!DOCTYPE HTML >
+
+<html>
+	<head>
+		<title>FORUM G1b uzuuzu</title>
+		<meta charset="utf-8"/>
+		<LINK REL=STYLESHEET href="forum.css">
+
+	</head>	
+
+	<body>
+		<div id='d1'>FORUM trop cool</div>
+		<table class="t1">
+			<tr>
+				<td class="ce1">HIGHWAY TO BANDS FORUM</td>
+				<td class="ce2">SUJETS</td>
+				<td class="ce3">MESSAGES</td>
+				<td class="ce4">DERNIER MESSAGE</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td >
+					<table>
+						<tr>
+          					<td rowspan=2 ><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Les concerts c'est cool</a></td>
+							
+    					</tr>
+     					<tr>
+          					<td class="a2">Parlez de vos concerts préferés
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>154</p></td>
+				<td>1624</td>
+				<td>ça commence a ressembler a quelque chose</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">La musique c'est génial</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Parlez de vos groupes préferés 
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>258</td>
+				<td>2695</td>
+				<td>Avoue c'est pas mal</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Le Rap</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Que ce soit Rap Us, ou pas, ici parlez librement
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>111</td>
+				<td>1897</td>
+				<td>wesh wesh </td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>					
+					<table>
+						<tr>
+						
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Festival en folie</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Cette année QUEEN is back
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>178</td>
+				<td>1268</td>
+				<td>olivier_du_36 dit "Hey"</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>					
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Groupes amateurs</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Faites decouvrir vos groupes préferés
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>175</td>
+				<td>1018</td>
+				<td>KEVIN dit "quelqu'un veut jouer dans mon groupe de metal ???"</td>
+			</tr>
+		</table>
+
+		<table class="t1">
+			<tr>
+				<td class="ce1">SUPPORT TECHNIQUE</td>
+				<td class="ce2">SUJETS</td>
+				<td class="ce3">MESSAGES</td>
+				<td class="ce4">DERNIER MESSAGE</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td >
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">BUGS</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Je n'arrive pas a cliquer
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>154</td>
+				<td >1624</td>
+				<td>J'aime YANIS BENNOUALA</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Aide et demande d'aide</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Je suis daltonien et je pue la merde
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>254</td>
+				<td>2375</td>
+				<td>je suis trop chauud</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Suggestions</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Donnez nous vos idées
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>124</td>
+				<td>1137</td>
+				<td>j'avoue on m'a un peu aidé mais quand meme !</td>
+			</tr>
+		</table>
+
+		<table class="t1">
+			<tr>
+				<td class="ce1">ARCHIVES</td>
+				<td class="ce2">SUJETS</td>
+				<td class="ce3">MESSAGES</td>
+				<td class="ce4">DERNIER MESSAGE</td>
+			</tr>
+			<tr class="c1" onmouseover="javascript:this.className='c1h';" onmouseout="javascript:this.className='c1';">
+				<td>
+					<table>
+						<tr>
+          					<td rowspan=2><img src="icone.png" class="img"></td>
+        					<td class="a1"><a href="" class="lien">Archives</a></td>
+    					</tr>
+     					<tr>
+          					<td class="a2">Ici sont archivés tous les messages anciens
+							<br />	 Modérateurs : <a href="" class="a3">Krakor</a>  <a href="" class="a3">ThiBal</a></td>
+   						</tr> 
+					</table>
+				</td>
+				<td>154</td>
+				<td>1624</td>
+				<td>Jean dit "c'est vraiment tres dur !!!" le 03/05 à 10:48</td>
+			</tr>
+		</table>
+	</body>
+</html>
