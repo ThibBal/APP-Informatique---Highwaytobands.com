63aadde6cea142e4e2806a7d4df042898b99c776
Yanis Benneouala <devil00735@MacBook-Pro-de-Yanis.local>
2014-05-06 09:56:00 +0200

    formulaireconcert

---
+++ HTB/vues/concert.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/inscription_style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="img/favicon.ico" />
+	<title>Highway To Bands</title>    
+</head>
+
+<body>
+	<?php include 'controleurs/header.php' ?>
+	<div id="inscription">
+		<form action="index.php?page=concert" method="post"> 
+			<span id="blabla">Inscription : Veuillez compléter les champs suivants</span>
+	<ul id="formulaire">
+		<li class="champs"><span>Nom : </span><input name=login class= "box" type="text"></li>
+		<li class="champs"><span>Salle : </span><input name=salle class="box" type="text"></li>
+		<li class="champs"><span>Artiste : </span><input name=artist class= "box" type="text"></li>
+		<li class="champs"><span>Date : </span><input name=zipcode class="box" type="text"></li>
+		<li class="champs"><span>Description : </span><input name=description class="box" type="text"></li>
+		<li class="champs"><span>Prix : </span><input name=photo class="box" type="text" /></li>
+		<li class="CGU"><span>J'accepte les <a href="conditions.php">Conditions Générales d'Utilisation</a></span><input type="checkbox" name="CGU" value="checked"/></li>
+		
+	</ul>
+
+	<div id="envoi"><input type="submit" value="S'inscrire" /></div>
+
+
+	</form>
+</div>
+<?php include 'controleurs/footer.php' ?>	
+	</body>
+		</html>
