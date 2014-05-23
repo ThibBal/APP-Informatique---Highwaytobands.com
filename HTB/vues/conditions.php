f63ef89954739849ab5255873d4f35b38c8302e4
Paul M-F <pmathiex-fortunet@isep.fr>
2014-05-23 13:57:21 +0200

    Condtions dutilisation

---
+++ HTB/vues/conditions.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/conditions.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="img/favicon.ico" />
+	<title>Highway To Bands</title>    
+</head>
+
+<body>
+	<?php include 'controleurs/header.php' ?>
+	<?php if(!isset($_SESSION['login'])){ ?>
+	
+	</div></a>
+	<?php } ?>
+	<div id="contenu">
+		<div class="article">
+			<h1 class="grostitre">Conditions d'utilisation</h1>
+			<div> <span class="titre"><p>Accès et utilisation du site et des données disponibles sur le site</p> </span>
+			<p class="texte">Les équipements (ordinateurs, téléphone, logiciels, moyens de télécommunication, ...) permettant l'accès au Site sont
+			   à la charge exclusive de l'Utilisateur, de même que les frais de télécommunications induits par leur utilisation.<br/>
+			   <br/>
+			   Lors de son inscription en ligne, l'Utilisateur s'engage à fournir des informations, exactes et complètes comme
+			   demandées dans le Formulaire d'inscription et donc à les tenir à jour sans délais.<br/>
+			   <br/>
+			   Dans l'hypothèse où l'Utilisateur fournirait des informations, inexactes, périmées ou incomplètes, la Société se réserve
+			   le droit de suspendre ou de résilier, dans les conditions visées aux présentes Conditions Générales d'Utilisation,
+			   l'accès à tout ou partie du Site.<br/>
+			   <br/>
+			   Les mineurs sont admis à s'inscrire sur le Site, à la condition qu'ils aient préalablement obtenu du(des) titulaire(s) de
+			   l'autorité parentale les concernant, l'autorisation de le faire et que le(les) titulaire(s) de l'autorité parentale a(ont)
+			   accepté d'être garant(s) du respect par l'utilisateur mineur des Conditions Générales d'Utilisation. Toute utilisation du
+			   Site par un utilisateur mineur est effectuée sous l'entière responsabilité du(des) titulaire(s) de l'autorité parentale 
+			   sur l'utilisateur mineur concerné.</p>
+			 </div>
+			
+			<div> <span class="titre"><p>Règles d'usage de l'internet</p> </span>
+			<p class="texte">La Société est soumise à une obligation de moyens. En conséquence, elle s'engage à faire ses meilleurs 
+			   efforts pour sécuriser l'accès, la consultation et l'utilisation du Site conformément aux règles d'usages de l'Internet.<br/>
+			   <br/>
+			   Le Site est accessible 24 heures sur 24, 7 jours sur 7 sauf en cas de force majeure ou de survenance d'un événement hors du 
+			   contrôle de la Société et sous réserve des éventuelles pannes et interventions de maintenance nécessaires au bon fonctionnement 
+			   du Site.<br/>
+			   <br/>
+			   Les interventions de maintenance pourront être effectuées sans que les Utilisateurs aient été préalablement avertis.<br/>
+			   <br/>
+			   L'Utilisateur déclare accepter les caractéristiques et les limites de l'Internet, et en particulier reconnaître que : <br/>
+			   <br/>
+			   		Son utilisation du Site se fait à ses risques et périls ; le Site lui est accessible "en état" et en fonction de 
+			   		sa disponibilité.<br/>
+			   		
+			   		Tout matériel téléchargé par l'Utilisateur ou obtenu de toute autre manière lors de l'utilisation du Site, l'est à 
+			   		ses risques et périls ; l'Utilisateur est seul responsable pour tout dommage subi par son ordinateur ou toute perte de 
+			   		données consécutif au téléchargement de ce matériel ou à la consultation du Site.<br/>
+			   		
+			   		Il lui appartient donc de prendre toutes les mesures appropriées de façon à protéger ses propres données et/ou 
+			   		logiciels de la contamination par des éventuels virus circulant à travers le Site et/ou les Informations.<br/>
+			   		
+			   		Aucun conseil et aucune information, qu'ils soient oraux ou écrit, obtenus par l'Utilisateur du Site ou lors de 
+			   		l'utilisation du Site ne sont susceptibles de créer des garanties non expressément prévues par les présentes Conditions 
+			   		Générales d'Utilisation. L'Utilisateur est seul responsable de l'utilisation qu'il fait des Informations présentes sur 
+			   		le Site et, en conséquence, la Société ne saurait être tenue responsable de dommages directs ou indirects découlant de 
+			   		l'utilisation de ces Informations.<br/>
+			   		
+			   		Il est seul responsable de l'utilisation du contenu des sites ayant un lien hypertexte avec le Site, la Société 
+			   		déclinant toute responsabilité quant à ces contenus.<br/>
+			   		
+			   		Il a connaissance de la nature de l'Internet, en particulier de ses performances techniques et des temps de réponse
+			   		 pour consulter, interroger ou transférer les Informations.<br/>
+			   		
+			   		La communication de ses Codes d'accès ou d'une manière générale de toute information jugée confidentielle est faite
+			   		 sous sa propre responsabilité.<br/>
+			   		
+			   		il lui appartient de prendre toutes mesures nécessaires pour s'assurer que les caractéristiques techniques de son 
+			   		équipement lui permettent la consultation des Informations.</p>
+			 </div>
+			 
+			 <div> <span class="titre"><p>Utilisation des données personnelles et respect de la vie privée</p> </span>
+			<p><span class="soustitre"> Codes d'accès</span> <br/>
+			<span class="texte">Les équipements (ordinateurs, téléphone, logiciels, moyens de télécommunication, ...) permettant l'accès au Site sont
+			   à la charge exclusive de l'Utilisateur, de même que les frais de télécommunications induits par leur utilisation.<br/>
+			   <br/>
+			   Lors de son inscription en ligne, l'Utilisateur s'engage à fournir des informations, exactes et complètes comme
+			   demandées dans le Formulaire d'inscription et donc à les tenir à jour sans délais.<br/>
+			   <br/>
+			   Dans l'hypothèse où l'Utilisateur fournirait des informations, inexactes, périmées ou incomplètes, la Société se réserve
+			   le droit de suspendre ou de résilier, dans les conditions visées aux présentes Conditions Générales d'Utilisation,
+			   l'accès à tout ou partie du Site.<br/>
+			   <br/>
+			   Les mineurs sont admis à s'inscrire sur le Site, à la condition qu'ils aient préalablement obtenu du(des) titulaire(s) de
+			   l'autorité parentale les concernant, l'autorisation de le faire et que le(les) titulaire(s) de l'autorité parentale a(ont)
+			   accepté d'être garant(s) du respect par l'utilisateur mineur des Conditions Générales d'Utilisation. Toute utilisation du
+			   Site par un utilisateur mineur est effectuée sous l'entière responsabilité du(des) titulaire(s) de l'autorité parentale 
+			   sur l'utilisateur mineur concerné.</span>
+			   </p>
+			 </div>
+
+		</div>
+
+
+			</form>
+			</p>
+		</div>
+
+	</div>
+
+	
+	<?php include 'controleurs/footer.php' ?>		
+</body>
+</html>
