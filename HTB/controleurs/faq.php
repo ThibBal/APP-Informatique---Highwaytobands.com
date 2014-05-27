7e04a550cf4ba81c216bb1204a4a33d31bf537dd
Yanis Benneouala <devil00735@MacBook-Pro-de-Yanis.local>
2014-05-27 11:06:41 +0200

    faq

---
+++ HTB/controleurs/faq.php
@@ -0,0 +1,1 @@
+<?php include 'vues/faq.php' ?>
---
+++ HTB/vues/faq.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/faq.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="img/favicon.ico" />
+	<title>Highway To Bands</title>    
+</head>
+
+<body>
+	<?php include 'controleurs/header.php' ?>
+	<?php if(!isset($_SESSION['login']))?>
+	
+	</div></a>
+	<?php  ?>
+	<div id="contenu">
+		<div class="article">
+			<p class="grostitre">FAQ</p>
+		</div>
+
+
+		
+			</p>
+		</div>
+
+	</div>
+
+	
+	<?php include 'controleurs/footer.php' ?>		
+</body>
+</html>
